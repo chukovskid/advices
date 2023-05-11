0d83537ec0bb37d26028d07792fad1f287dc3747
Dimitar <dimitarch@iborn.net>
2023-05-12 00:48:52 +0200

    database ready

--- lib/screens/home/lawyerHomeWidget.dart
+++ lib/screens/home/lawyerHomeWidget.dart
@@ -161,7 +161,7 @@ class _LawyerHomeWidgetState extends State<LawyerHomeWidget>
             context,
             MaterialPageRoute(
                 builder: (context) =>
-                    IframeWidget(src: 'https://doc-ai-ten.vercel.app/'))),
+                    IframeWidget(src: 'https://doc-ai-two.vercel.app/'))),
       },
       child: MouseRegion(
         onEnter: (PointerEvent details) {
@@ -442,7 +442,7 @@ class _LawyerHomeWidgetState extends State<LawyerHomeWidget>
                         context,
                         MaterialPageRoute(
                             builder: (context) =>
-                                IframeWidget(src: 'https://doc-ai-ten.vercel.app/',))),
+                                IframeWidget(src: 'https://doc-ai-two.vercel.app/',))),
                     child: Card(
                       color: advokatGreenColor,
                       elevation: 5,
